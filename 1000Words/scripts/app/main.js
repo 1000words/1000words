cae1ac4a560d2af4d835dda50c6e0fccdd15b1c9
Milos Bozic <mbozic@execom.eu>
2014-10-17 19:40:23 +0000

    Changes made by Milos Bozic (mbozic@execom.eu). everlive init

--- 1000Words/scripts/app/main.js
+++ 1000Words/scripts/app/main.js
@@ -1,21 +1,30 @@
-var app = (function(){
+var app = (function () {
+    var everlive = new Everlive({
+            apiKey: settings.everlive.apiKey,
+            scheme: settings.everlive.schema
+        });
+
     var mobileApp = new kendo.mobile.Application(document.body, {
-                                                     transition: 'slide',
-                                                     statusBarStyle: statusBarStyle,
-                                                     skin: 'flat'
-                                                 });
-    
-    var onDeviceReady = function(){ 
+        transition: 'slide',
+        statusBarStyle: statusBarStyle,
+        skin: 'flat'
+    });
+
+    var onDeviceReady = function () {
         //mobileApp.navigate('views/splashScreen.html');
-        setTimeout(function(){
+        setTimeout(function () {
             mobileApp.navigate('views/mapExplorerView.html');
         }, 1000);
         
         navigator.splashscreen.hide();
     }
-    
+
     var os = kendo.support.mobileOS,
         statusBarStyle = os.ios && os.flatVersion >= 700 ? 'black-translucent' : 'black';
-    
+
     document.addEventListener('deviceReady', onDeviceReady, false);
+
+    return {
+        everlive: everlive
+    }
 })();
