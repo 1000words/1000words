07123470660eaf6907200155c8d24f688a9a6ead
Luka Ranisavljevic <lranisavljevic@execom.eu>
2014-10-18 20:29:44 +0000

    Changes made by Luka Ranisavljevic (lranisavljevic@execom.eu).

--- 1000Words/scripts/app/notificationWindowViewModel.js
+++ 1000Words/scripts/app/notificationWindowViewModel.js
@@ -12,14 +12,39 @@ app.NotificationWindowViewModel = (function() {
         };
         
         var reply = function() {
-            alert('Open, camera!')
+            takePicture();
             return false;
-        }
+        };
         
         var dismiss = function() {
             app.mobileApp.navigate('#:back');
             return false;
-        }
+        };
+        
+        var takePicture = function() {
+            navigator.camera.getPicture(function (data) {
+                var filename = Math.random().toString(36).substring(2, 15) + ".jpg";    
+                app.everlive.Files.create({
+                    Filename: filename,
+                    ContentType: "image/jpeg",
+                    base64: data
+                }).then(function() {
+                    $.get('http://api.everlive.com/v1/' + settings.Settings.everlive.apiKey 
+                          + '/functions/sendPhoto?to=' + app.NotificationWindowViewModel.activeNotification.payload.message.DeviceId 
+                          + '&imageName=' + filename 
+                          + '&sender=' + device.uuid);
+                    app.mobileApp.navigate('#:back');
+                });
+                },
+                function(msg) {
+                    alert(msg);
+                }, 
+                {
+                    destinationType: Camera.DestinationType.DATA_URL,
+                    targetHeight: 400,
+                    targetWidth: 400
+                });
+        };
         
         return {
             init: init,
@@ -32,3 +57,4 @@ app.NotificationWindowViewModel = (function() {
     
     return viewModel;
 })();
+
