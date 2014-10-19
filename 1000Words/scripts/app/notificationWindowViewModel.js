eecdb81a30a8e2258b5f1ea210524a669578f1b4
Milos Bozic <mbozic@execom.eu>
2014-10-19 00:48:43 +0000

    Changes made by Milos Bozic (mbozic@execom.eu).

--- 1000Words/scripts/app/notificationWindowViewModel.js
+++ 1000Words/scripts/app/notificationWindowViewModel.js
@@ -2,56 +2,66 @@ var app = app || {};
 
 app.NotificationWindowViewModel = kendo.observable({
     activeNotification: {},
-    
+
     isProgressVisible: false,
-    
+
     progressText: '',
-    
-    init: function(){
+
+    init: function () {
+        app.NotificationWindowViewModel.initMap();
     },
-    
-    show: function() {
-    },
-    
-    reply: function() {
+
+    show: function () {},
+
+    reply: function () {
         this.takePicture();
         return false;
     },
-    
-    dismiss: function() {
+
+    dismiss: function () {
         app.mobileApp.navigate('#:back');
         return false;
     },
-    
-    takePicture: function() {
+
+    initMap: function () {
+        
+        var mapProp = {
+            center: new google.maps.LatLng(0, 0),
+            zoom: 2,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        map = new google.maps.Map(document.getElementById("googleMapView"), mapProp);
+
+        google.maps.event.addListener(map, 'zoom_changed', function () {
+            showVisibleCities(360 - previousCompassAngle);
+        });
+    },
+
+    takePicture: function () {
         var that = this;
         navigator.camera.getPicture(function (data) {
-            that.set('isProgressVisible', true);
-            that.set('progressText', 'Sending picture...');
-            var filename = Math.random().toString(36).substring(2, 15) + ".jpg";
-            app.everlive.Files.create({
-                Filename: filename,
-                ContentType: "image/jpeg",
-                base64: data
-            }).then(function() {
-                $.get('http://api.everlive.com/v1/' + settings.Settings.everlive.apiKey 
-                      + '/functions/sendPhoto?to=' + app.NotificationWindowViewModel.activeNotification.payload.message.DeviceId 
-                      + '&imageName=' + filename 
-                      + '&sender=' + device.uuid);
-                
-                that.set('isProgressVisible', false);
-                app.mobileApp.navigate('#:back');
-            });
+                that.set('isProgressVisible', true);
+                that.set('progressText', 'Sending picture...');
+                var filename = Math.random().toString(36).substring(2, 15) + ".jpg";
+                app.everlive.Files.create({
+                    Filename: filename,
+                    ContentType: "image/jpeg",
+                    base64: data
+                }).then(function () {
+                    $.get('http://api.everlive.com/v1/' + settings.Settings.everlive.apiKey + '/functions/sendPhoto?to=' + app.NotificationWindowViewModel.activeNotification.payload.message.DeviceId + '&imageName=' + filename + '&sender=' + device.uuid);
+
+                    that.set('isProgressVisible', false);
+                    app.mobileApp.navigate('#:back');
+                });
             },
-            function(msg) {
+            function (msg) {
                 that.set('isProgressVisible', false);
                 //alert(msg);
-            }, 
-            {
+            }, {
                 destinationType: Camera.DestinationType.DATA_URL,
                 targetHeight: 400,
                 targetWidth: 400
             });
-    }  
+    }
 });
-
